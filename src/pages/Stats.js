090f6872e7680915d2d606090a99074ef49bd2bd
N-Shar-ma <bablinaneh@gmail.com>
2021-05-08 01:50:54 +0530

    added basic stats graph

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default function Stats({ totalCount, correctCount }) {
+    const graphWidth = correctCount / totalCount * 100;
     return (
         <section>
             <h1>Stats</h1>
             Out of {totalCount} you got {correctCount} right!
+            {totalCount && 
+            <div style={{backgroundColor: "red", height: "20px", width: "clamp(250px, 80%, 600px)"}}>
+                <div style={{backgroundColor: "green", height: "100%", width: `${graphWidth}%`}}>
+                </div>
+            </div>}
         </section>
     )
 }
